d705a016caa610155dc050d329a9b14fb1a593f8
CodeWith-Het <hetp1065@gmail.com>
2025-10-22 02:50:30 -0500

    Task 5 - Javascripts Questions sloved

--- Cohrot Task/Task 5/script.js
+++ Cohrot Task/Task 5/script.js
@@ -1,4 +1,4 @@
-Q1)Write an arrow function that returns the product of two numbers.
+--->Q1)Write an arrow function that returns the product of two numbers.
 
 const add = (a,b) =>{
     return a*b;
@@ -6,7 +6,7 @@ const add = (a,b) =>{
 
 console.log(add(3,7));
 
-Q2)Write a function declaration to check if a number is even or odd.
+--->Q2)Write a function declaration to check if a number is even or odd.
 
 function checkEvenOddNumber(val){
     if(val % 2===0){
@@ -19,7 +19,7 @@ function checkEvenOddNumber(val){
 checkEvenOddNumber(5);
 checkEvenOddNumber(10);
 
-Q3)Write a function expression that takes a name and returns "Hello, name!"
+--->Q3)Write a function expression that takes a name and returns "Hello, name!"
 
 let fnc = function(nameString){
     console.log(`Hello, ${nameString}`);
@@ -27,7 +27,7 @@ let fnc = function(nameString){
 
 fnc("Het");
 
-Q4)Write a function with default parameter that greets a user by name or "Guest" if no name is given.
+--->Q4)Write a function with default parameter that greets a user by name or "Guest" if no name is given.
 
 function greeting (name="Guest"){
     console.log(`Hello, ${name}`);
@@ -36,13 +36,13 @@ function greeting (name="Guest"){
 greeting(); // by defualt ye Guest le lega jab mene koi argument pass nahi kiya hoga tab 
 greeting("Het");
 
-Q5)Use a for loop to print numbers from 1 to 5.
+--->Q5)Use a for loop to print numbers from 1 to 5.
 
 for(let i=1;i<=5;i++){
     console.log(i);
 }
 
-A6)Use a while loop to print all even numbers less than 10.
+--->Q6)Use a while loop to print all even numbers less than 10.
 
 let i=1;
 while(i<=9){
@@ -52,7 +52,7 @@ while(i<=9){
     i++;
 }
 
-Q7)Write a function that takes any number of arguments and returns their sum (use rest parameters).
+--->Q7)Write a function that takes any number of arguments and returns their sum (use rest parameters).
 
 function add(...val){
    let total = 0;
@@ -64,7 +64,7 @@ function add(...val){
 
 console.log(add(1,2,5,7,4,9,5,6,2,4,7));
 
-Q8)Write an arrow function to find the square of a number.
+--->Q8)Write an arrow function to find the square of a number.
 
 const squareOfNumber = (value) =>{
     return value ** 2;
