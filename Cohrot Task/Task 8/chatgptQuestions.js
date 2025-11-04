2f9b30c75fa60849242e5ea0fc44190471787259
CodeWith-Het <hetp1065@gmail.com>
2025-11-03 23:52:31 -0600

    inital commit - 4/11/2025
    js questions sloved

--- Cohrot Task/Task 8/chatgptQuestions.js
+++ Cohrot Task/Task 8/chatgptQuestions.js
@@ -75,3 +75,50 @@ div.forEach(element => {
     element.style.backgroundColor="green";
     element.style.backgroundColor="blue";
 });
+
+
+---> Write JavaScript code to get the visible text content (excluding hidden text and HTML tags) of an element using innerText.
+let span = document.querySelector("#spanText");
+span.innerText="Hello ji";
+
+--->Update the text of a paragraph using innerText and observe what happens to its child elements.
+let p = document.querySelector("#pText");
+p.innerText="Hello ji kaise ho aap Mithai khai?";
+
+---->Use textContent to get the full text content of an element including all hidden text, ignoring HTML tags
+console.log(document.getElementById("myDiv").textContent);
+
+---->Demonstrate with an example how textContent handles spaces and line breaks differently than innerText
+let p = document.getElementById("textBro");
+p.textContent="       bro kaise ho";
+
+---->Write code to retrieve all text inside a parent element using textContent including its child elements.
+let childDiv=document.querySelector("#childDiv");
+childDiv.textContent="Me hu child Div";
+
+---->Change the text content of an element using textContent without affecting its HTML structure.
+let myDiv = document.querySelector("#myChild2");
+myDiv.textContent="Hello Brother big Fan";
+
+---->Write JavaScript to dynamically insert HTML content like headings and paragraphs inside a div using innerHTML.
+let myDiv = document.querySelector("#mainDiv");
+myDiv.innerHTML = `<h1>Hello ji kaise ho aap lok</h1>
+<p>jay Shree Ram</p>`;
+
+---> Use innerHTML to add new list items to an unordered list dynamically.
+let ul = document.querySelector("#myUl");
+ul.innerHTML=`<li>Home</li> 
+<li>About</li>
+<li>Product</li>`
+
+----> Write code to get the full HTML content inside an element using innerHTML.
+let html = document.querySelector(".myH1");
+html.innerHTML=` <h1 id="title"></h1>
+    <p id="desc">Welcome!</p>
+    <button id="clickBtn">click me</button>
+
+    <div class="box">box1</div>
+    <div class="box">box2</div>
+    <div class="box">box3</div>
+    <div class="box">box4</div>
+    <div class="box">box5</div>`
